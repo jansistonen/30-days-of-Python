f02153e57eac33dd7432a32e5fdc17138f2c9385
Jan Sistonen <jsistone@uef.fi>
2025-12-20 18:37:30 +0200

    new try 2nd commit

---
+++ day-3/main.py
@@ -0,0 +1,12 @@
+age = input("Enter age:")
+print(age)
+
+my_height = input("Enter your height: ")
+print(my_height)
+
+base = input("Triangle base: ")
+height = input("Triangle height")
+baseToFloat = float(base)
+heightToFloat = float(height)
+triangle_area = 0.5 * baseToFloat * heightToFloat
+print(triangle_area)

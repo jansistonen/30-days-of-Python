3832d00eed8266631ed46f319411a63b4bacd74e
Jan Sistonen <jsistone@uef.fi>
2025-12-21 11:08:04 +0200

    3rd commit

---
+++ day-3/task5.py
@@ -0,0 +1,27 @@
+"""a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+per = a+b+c
+print("The perimeter of triangle is: ", per)
+
+#task 6
+len = int(input("Give length of an rectangle: "))
+wid = int(input("Give width of an rectangle: "))
+
+area = len * wid
+perimeter = 2*len + 2*wid
+print("Area is: ", area, " - Perimeter is: ", perimeter)
+
+#task 7
+rad = int(input("Give radius for circle: "))
+area = 3.14 * (rad**2)
+circumference = 2 * 3.14 * rad
+print("Area of a circle is: ", area, " - Circumference is: ", circumference)"""
+
+#task12
+a = len("Python")
+b = len("Dragon")
+print(a," ", b)
+print(a == b)
+
+

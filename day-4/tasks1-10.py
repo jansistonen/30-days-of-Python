80119a2d9445ff9b939365ca54c346aab1727d31
Jan Sistonen <jsistone@uef.fi>
2025-12-21 19:19:52 +0200

    day4 tasks till #17

--- day-4/tasks1-10.py
+++ day-4/tasks1-10.py
@@ -43,5 +43,30 @@ print(company[0:6])
 #Task 10 - Check if Coding For All string contains a word Coding using the method index, find or other methods.
 print(company.find("Coding")) #find() returns the index for the word, returns -1 if not found
 
+#Task 11 - Replace the word coding in the string 'Coding For All' to Python.
 strings[0] = "Replace"
 print(strings)
+
+#Task 12 - Change Python for Everyone to Python for All using the replace method or other methods.
+sentence = "Python for Everyone"
+sentence2 = sentence.replace('Everyone', 'All')
+print(sentence2)
+
+#Task 13 - Split the string 'Coding For All' using space as the separator (split()) .
+splitted = company.split()
+print(splitted)
+
+#Task 14 - "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+string_14 = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+splitted_string_14 = string_14.split(', ')
+print(splitted_string_14)
+
+#Task 15 - What is the character at index 0 in the string Coding For All.
+print(company[0])
+
+#Task 16 - What is the last index of the string Coding For All.
+print(company[-1])
+print(company[len(company)-1])
+
+#Task 17 - What character is at index 10 in "Coding For All" string.
+print(company[10])

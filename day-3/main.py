952183cf17f3fdb4f992f54711e7829ef3ce4e80
Jan Sistonen <jsistone@uef.fi>
2025-12-22 12:05:08 +0200

    day5 1st

--- day-3/main.py
+++ day-3/main.py
@@ -5,7 +5,7 @@ my_height = input("Enter your height: ")
 print(my_height)
 
 base = input("Triangle base: ")
-height = input("Triangle height")
+height = input("Triangle height: ")
 baseToFloat = float(base)
 heightToFloat = float(height)
 triangle_area = 0.5 * baseToFloat * heightToFloat
---
+++ day-5/tasks1-10.py
@@ -0,0 +1,27 @@
+list = list()
+list = ['banana','orange','mango','lemon','lime']
+print(list)
+
+#task 3
+length_of_list = len(list)
+print(length_of_list)
+
+#task 4 - Get the first item, the middle item and the last item of the list
+first = list[0]
+last = list[len(list)-1]
+middle_index = int(len(list) / 2)
+middle = list[middle_index]
+
+print(first, last, middle)
+
+#task 5 - Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+
+mixed_data_types = ["Jan", 31, 174, "In relationship", "Streetname 21"]
+
+#task 6 - Declare a list variable named it_companies and assign initial values 
+# Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+#Task 7 - print list
+print(mixed_data_types)
+print(it_companies)

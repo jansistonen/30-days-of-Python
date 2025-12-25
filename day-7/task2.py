f0f8e75b0023185201af217dc0de1c47fadc8de5
Jan Sistonen <jsistone@uef.fi>
2025-12-25 14:43:14 +0200

    day7, exercise 2 started

---
+++ day-7/task2.py
@@ -0,0 +1,19 @@
+#Exercises: Level 2
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+#Join A and B
+A.union(B)
+print(A.union(B))
+#Find A intersection B
+print(A.intersection(B))
+#Is A subset of B
+print(A.issubset(B))
+#Are A and B disjoint sets
+print(A.isdisjoint(B))
+#Join A with B and B with A
+#What is the symmetric difference between A and B
+#Delete the sets completely

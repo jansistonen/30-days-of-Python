df0015c8acc8f50a0f4980dad7112f479b9a2c2f
Jan Sistonen <jsistone@uef.fi>
2025-12-27 19:02:36 +0200

    first task on dictionaries

---
+++ day-8/task1.py
@@ -0,0 +1,31 @@
+#Create an empty dictionary called dog
+dog = {}
+#Add name, color, breed, legs, age to the dog dictionary
+dog['name'] = 'value1'
+dog['color'] = 'value2'
+dog['breed'] = 'value3'
+dog['legs'] =  'value4'
+dog['age'] = 0
+print(dog)
+#Create a student dictionary and add
+#first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
+student = {
+    'first name':'value1',
+    'last_name':'value2',
+    'gender':'value3',
+    'age':'value4',
+    'status':'value5',
+    'skills':'value6',
+    'country':'value7',
+    'city':'value8',
+    'address':'value9',
+    }
+print(student)
+#Get the length of the student dictionary
+#Get the value of skills and check the data type, it should be a list
+#Modify the skills values by adding one or two skills
+#Get the dictionary keys as a list
+#Get the dictionary values as a list
+#Change the dictionary to a list of tuples using items() method
+#Delete one of the items in the dictionary
+#Delete one of the dictionaries

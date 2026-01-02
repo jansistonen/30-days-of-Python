2ec0d24081b3970d2da13407c217b736cabf6748
Jan Sistonen <jsistone@uef.fi>
2026-01-02 17:45:48 +0200

    degree convert task  cleared

--- day-11/task-lvl-1.py
+++ day-11/task-lvl-1.py
@@ -18,7 +18,7 @@ print(circle_area(r=50))
 def add_all_nums(para1):
     summed = 0
     for i in para1:
-        if type(i) == int:
+        if type(i) == int: #add only if the input is integer
             summed = summed + i
         else:
             print(f'{i} is Not an number!')
@@ -26,7 +26,13 @@ def add_all_nums(para1):
 print(add_all_nums([1,2,3,4,5,6, 'a']))
 
 
-# 4 - Temperature in °C can be converted to °F using this formula: °F = (°C x 9/5) + 32. Write a function which converts °C to °F, convert_celsius_to-fahrenheit.
+# 4 - Temperature in °C can be converted to °F using this formula: °F = (°C x 9/5) + 32.
+#  Write a function which converts °C to °F, convert_celsius_to-fahrenheit.
+def cel_to_fah(degree_cel):
+    degree_fahr = degree_cel * (9/5) + 32
+    return degree_fahr
+input_from_user = int(input('Give a temperature in Celcius: '))
+print(f'You entered a value of {input_from_user} celcius. That is {cel_to_fah(input_from_user)} in Fahrenheit')
 # 5 - Write a function called check-season, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
 # 6 - Write a function called calculate_slope which return the slope of a linear equation
 # 7 - Quadratic equation is calculated as follows: ax² + bx + c = 0. Write a function which calculates solution set of a quadratic equation, solve_quadratic_eqn.

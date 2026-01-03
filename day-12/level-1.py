b664ed31004bc7a982efbd646c9ccc3390de28fd
Jan Sistonen <jsistone@uef.fi>
2026-01-03 11:56:20 +0200

    leve1 second task done!

--- day-12/level-1.py
+++ day-12/level-1.py
@@ -5,13 +5,22 @@
 from random import random, randint
 import string
 
-def random_user_id():
+def random_user_id(para1):
     id = []
     char_in_use = string.ascii_letters + string.digits
-    print(char_in_use)
-    for i in range(6):
+    for i in range(para1):
         id.append(char_in_use[randint(0,len(char_in_use)-1)])
     res = ''.join(id)
     return res
-print(random_user_id())
+#print(random_user_id())
 
+#Modify the previous task. Declare a function named user_id_gen_by_user. It doesn’t take any parameters but 
+# it takes two inputs using input(). One of the inputs is the number of characters and the second input is 
+# the number of IDs which are supposed to be generated.
+def user_id_gen_by_user():
+    lenght = int(input('Give a length of an id: '))
+    count = int(input("How many id's will be generated: "))
+
+    for i in range(count):
+        print('#', random_user_id(lenght))
+user_id_gen_by_user()

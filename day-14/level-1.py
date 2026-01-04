75cfbcc79ebc0d50248fd3d61d20d891c81bd090
Jan Sistonen <jsistone@uef.fi>
2026-01-04 12:45:25 +0200

    new task cleared, filter()

--- day-14/level-1.py
+++ day-14/level-1.py
@@ -8,12 +8,39 @@ for i in countries:
     print(i)
 
 # LEVEL 2
-# Use map to create a new list by changing each country to uppercase in the countries list
+# 1 - Use map to create a new list by changing each country to uppercase in the countries list
 def to_upper(country):
     return country.upper()
 
 countries_to_upper = map(to_upper, countries)
 print(list(countries_to_upper))
 
+# 2 - Use map to create a new list by changing each number to its square in the numbers list
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def f(number):
+    return number * number
+square_numbers = map(f, numbers)
+print(list(square_numbers))
+
+# 3 - Use map to change each name to uppercase in the names list
+
 names_to_upper = map(to_upper, names)
 print(list(names_to_upper))
+
+# 4 - Use filter to filter out countries containing 'land'.
+def y(country):
+    if 'land' in country:
+        return True
+    else:
+        return False
+land_countries = filter(y, countries)
+print(list(land_countries)) # should print only countries that include 'land'
+
+# 5 - Use filter to filter out countries having exactly six characters.
+def is_six(country):
+    if len(country) == 6:
+        return True
+    else:
+        return False
+countries_of_len = filter(is_six, countries)
+print(list(countries_of_len))

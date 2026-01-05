8636d93807451c231df12fbc4362aa372989d766
Jan Sistonen <jsistone@uef.fi>
2026-01-05 10:49:53 +0200

    tasks form day17 done, had to get help from chat to unpacking task

---
+++ day-17/task.py
@@ -0,0 +1,8 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+#Unpack the first five countries and store them in a variable nordic_countries,
+#  store Estonia and Russia in es, and ru respectively.
+
+nordic_countries, es, ru = names[:5], names[5], names[6]
+print((nordic_countries))
+print(es)
+print(ru)

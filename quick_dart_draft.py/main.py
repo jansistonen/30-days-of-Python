9ef2a8ae445638cd9325a00c9a90d1a75c946127
Jan Sistonen <jsistone@uef.fi>
2026-01-05 16:38:40 +0200

    json kirjoitus toimii (vain kierrokset) atm, seuraavaksi kirjoitetaan aina uusi_sessio, nyt kirjoittaa vain vanhan tiedoston päälle - ei kovin hyödyllistä

--- quick_dart_draft.py/main.py
+++ quick_dart_draft.py/main.py
@@ -1,5 +1,23 @@
 # tehdään skirpti joka tallentaa darts tuloksia, 3 heittoa / vuoro
 # jatketaan siten että lasketaan keskiarvo 'vapaalta kierrokselta'
+# nyt pisteet tallentuu vain ohjelmaan muttei tiedostoon, opetellaan tallentamaan kaikki json-filuun
+
+import json
+
+data = {
+    'sessio_id' : 'sessio1',
+    'kierrokset' : 0,
+    'kierros' : [],
+    'kierroksien_ka' : 0,
+    'heitetyt_tikat': 0
+}
+
+json_str = json.dumps(data, indent=4)
+with open('sample.json', 'w') as f:
+    f.write(json_str)
+
+print(json_str)
+
 kierroksia = 0
 lista_per_kierros = []
 lista_kierroksen_tulos = []
@@ -10,11 +28,14 @@ def kierros_count(para1):
         summa = summa + num
     #return summa / (len(para1)) #funktio palauttaa keskiarvon, joka tallennetaan toiseen listaan! HUOM. ka/ per tikka
     return summa
+
 def kierros_lista_läpikäynti(para2):
     #jotain lissää
     sum = 0
     for turn in lista_kierroksen_tulos:
         sum = sum + turn
+    if sum == 0:
+        return None
     return sum / len(lista_kierroksen_tulos)
 
 while input('Jatka heittoja painamalla k') == 'k':
@@ -33,6 +54,15 @@ while input('Jatka heittoja painamalla k') == 'k':
 print(f'Lopetit heittovuoron. Heitit yhteensä {kierroksia} kierrosta!')
 print(lista_kierroksen_tulos)
 print('Heittovuoron keskiarvo: ', kierros_lista_läpikäynti(lista_kierroksen_tulos))
+
+data['kierrokset'] = kierroksia
+data['heitetyt_tikat'] = 3 * kierroksia
+
+with open('sample.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
+print("Tallennettu sample.json")
+print(json.dumps(data, indent=4))
 
 #TODO 
 # - Aloita kierros 

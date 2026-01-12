ab6b50e7492ec7be6a578a6b40a8de42633a2de6
Jan Sistonen <jsistone@uef.fi>
2026-01-12 19:25:00 +0200

    learned numpy, used jupyter again

--- day-22/ex_day_22.py
+++ day-22/ex_day_22.py
@@ -9,7 +9,15 @@ from bs4 import BeautifulSoup
 url = 'http://www.bu.edu/president/boston-university-facts-stats/'
 
 response = requests.get(url)
+content = response.content
+soup = BeautifulSoup(content, 'html.parser')
 #check status
 status = response.status_code
 
 print(status)
+print(soup.title)
+
+tables = soup.find_all('h3')
+table = tables[0]
+
+print(tables)
---
+++ day-24/.ipynb_checkpoints/ex_24-checkpoint.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+print('Numpy version: ', np.__version__)
+print(dir(np))
